443d4e131cb0d1077065f8d3b0694cda00dcda75
Axel Pardemann <axelitusdev@gmail.com>
2021-01-22 22:56:39 -0600

    Add Sublime Text protocol handler

---
+++ protocol_handler.js
@@ -0,0 +1,56 @@
+function main(url)
+{
+    var protocols = {
+        subl: 'sublime_text',
+        sublime: 'sublime_text'
+    };
+    var regex = new RegExp('^(' + parseProtocols(protocols) + '):\\\/\\\/open\\\/?\\?(?:url=file:\\\/\\\/|file=)(.+)&line=(\\d+)$');
+    var match = regex.exec(url);
+
+    if(match) {
+        var handler = eval(readHandler(protocols[match[1]]));
+        execute(handler.command(decodeURIComponent(match[2]).replace(/\+/g, ' '), match[3]), handler.settings);
+    }
+}
+
+function parseProtocols(protocols)
+{
+    var parsed = '';
+    for(var protocol in protocols) {
+        parsed += '|' + protocol;
+    }
+
+    return parsed.substr(1);
+}
+
+function readHandler(app)
+{
+    var path = './'+ app + '/command.js';
+    var file_system = new ActiveXObject("Scripting.FileSystemObject");
+    if (file_system.fileExists(path)) {
+        return file_system.OpenTextFile(path, 1).ReadAll();
+    }
+}
+
+function execute(command, settings)
+{
+    if (command != '') {
+        var shell = new ActiveXObject('WScript.Shell');
+        shell.Exec(command);
+        shell.AppActivate(settings.window_title);
+    }
+}
+
+function readRegistryValue(key, defaultValue)
+{
+    var shell = new ActiveXObject('WScript.Shell');
+    var value = defaultValue;
+
+    try {
+        var value = shell.RegRead(key);
+    } catch (exception) {}
+
+    return value;
+}
+
+main(WScript.Arguments(0));
---
+++ sublime_text/command.js
@@ -0,0 +1,21 @@
+({
+    settings: {
+        window_title: 'Sublime Text'
+    },
+    command: function(file, line) {
+        var installLocationRegKey = 'HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Sublime Text 3_is1\\InstallLocation';
+        var command = '%editor% %file%:%line%';
+        var installPath = readRegistryValue(installLocationRegKey);
+
+        if (installPath == '') {
+            return;
+        }
+
+        command = command.replace(/%editor%/g, installPath + '/subl.exe')
+            .replace(/%file%/g, file)
+            .replace(/%line%/g, line)
+            .replace(/\//g, '\\');
+
+        return command;
+    }
+});
